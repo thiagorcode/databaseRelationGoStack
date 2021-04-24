5c5c9831eb68cefe9e623e48d42e081f12043ccf
Thiago <ti.thiago.rodrigues@outlook.com>
2021-04-23 23:27:28 -0300

    CreateUser Add

--- src/modules/customers/infra/typeorm/entities/Customer.ts
+++ src/modules/customers/infra/typeorm/entities/Customer.ts
@@ -6,15 +6,21 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
+@Entity('users')
 class Customer {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   email: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
---
+++ src/shared/infra/typeorm/migrations/1619225869070-CreateUser.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateUser1619225869070 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            isUnique: true,
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
